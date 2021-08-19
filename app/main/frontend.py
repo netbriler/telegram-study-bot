ed3eb36c42119ac258ad857e8f3a9e9d1b6c839b
netbriler <44978350+netbriler@users.noreply.github.com>
2021-08-19 11:45:52 +0000

    Build: (483e15e) Merge pull request #3 from netbriler/development
    Adaptive menu & logging page

--- app/main/frontend.py
+++ app/main/frontend.py
@@ -1,7 +1,15 @@
-from flask import current_app
+from flask import current_app, Response
 from flask_login import login_required
 
 from app.main import main
+
+
+@main.route('/logs')
+@login_required
+def logs():
+    with open(current_app.config['LOGGING_DIR'] + '/log.out', 'r') as f:
+        content = f.read()
+    return Response(content, mimetype='text/plain')
 
 
 @main.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
