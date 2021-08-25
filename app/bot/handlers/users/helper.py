ea64ded7cb17086b9ba7ba5ecf3e976561c78d05
netbriler <netbriler@gmail.com>
2021-08-25 13:14:17 +0300

    Profanity filter block users fix

--- app/bot/handlers/users/helper.py
+++ app/bot/handlers/users/helper.py
@@ -10,19 +10,20 @@ from ...keyboards.default import get_menu_keyboard_markup
 from ...loader import bot, current_app
 
 
-@bot.message_handler(content_types=['text'], func=lambda m: True)
-@base(send_chat_action=False)
-def get_all_messages(message: Message, current_user: User):
-    if message.chat.type == 'private':
-        text = 'Выберите действие в меню 👇'
+@bot.message_handler(content_types=['text'], func=lambda m: m.chat.type == 'private')
+@base()
+def _all_private_messages_handler(message: Message, current_user: User):
+    text = 'Выберите действие в меню 👇'
 
-        return send_message_private(message, text, reply_markup=get_menu_keyboard_markup(current_user.is_admin()))
+    send_message_private(message, text, reply_markup=get_menu_keyboard_markup(current_user.is_admin()))
 
-    if current_app.config['PROFANITY_FILTER']:
-        pf = ProfanityFilter()
-        answer_list = ['Фу! Как некультурно!', 'Мат - для лохов', 'Не матюкайся', 'Не матерись',
-                       'Мне показалось, или ты быканул?', 'Мат - плохо', 'Не матерись дуралей',
-                       'Не излагай свои мысли как прибей', 'Кто обзывается, тот сам так и называется']
 
-        if pf.is_profane(message.text):
-            send_message_private(message, random.choice(answer_list), reply_to_message_id=message.message_id)
+@bot.message_handler(content_types=['text'], func=lambda m: current_app.config['PROFANITY_FILTER'])
+def _profanity_filter(message: Message):
+    pf = ProfanityFilter()
+    answer_list = ['Фу! Как некультурно!', 'Мат - для лохов', 'Не матюкайся', 'Не матерись',
+                   'Мне показалось, или ты быканул?', 'Мат - плохо', 'Не матерись дуралей',
+                   'Не излагай свои мысли как прибей', 'Кто обзывается, тот сам так и называется']
+
+    if pf.is_profane(message.text):
+        send_message_private(message, random.choice(answer_list), reply_to_message_id=message.message_id)
