32b88e6ebcd2bfef2cafbde36437c03c8a619f69
netbriler <netbriler@gmail.com>
2022-02-01 20:46:15 +0200

    Add previous week homework

--- app/bot/keyboards/inline/week.py
+++ app/bot/keyboards/inline/week.py
@@ -1,11 +1,15 @@
 from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def get_week_inline_markup(query: str, next: bool = False) -> InlineKeyboardMarkup:
+def get_week_inline_markup(query: str, shift: str = 'this', with_previous: bool = False) -> InlineKeyboardMarkup:
     markup = InlineKeyboardMarkup()
 
-    this_week_title = 'На эту неделю' + ('*' if not next else '')
-    next_week_title = 'На следущую неделю' + ('*' if next else '')
+    this_week_title = 'На эту неделю' + ('*' if shift == 'this' else '')
+    next_week_title = 'На следущую неделю' + ('*' if shift == 'next' else '')
+
+    if with_previous:
+        previous_week_title = 'За прошлую неделю' + ('*' if shift == 'previous' else '')
+        markup.row(InlineKeyboardButton(previous_week_title, callback_data=f'{query}_previous'))
 
     markup.row(InlineKeyboardButton(this_week_title, callback_data=f'{query}_this'))
     markup.row(InlineKeyboardButton(next_week_title, callback_data=f'{query}_next'))
