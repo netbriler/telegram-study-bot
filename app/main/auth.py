b618d4801c804aae58af1f9dbd728baa732685e2
netbriler <44978350+netbriler@users.noreply.github.com>
2022-04-22 17:51:20 +0000

    Build: (90660d2) Add web app button for admins

--- app/main/auth.py
+++ app/main/auth.py
@@ -1,10 +1,10 @@
-from flask import redirect, abort, request, current_app, jsonify, render_template, url_for
+from flask import redirect, abort, request, current_app, jsonify, render_template, url_for, Response
 from flask_login import login_user, logout_user, login_required, current_user
 
 from app.bot.loader import bot
 from app.main import main
 from app.models import User
-from app.services.telegram_auth import verify_authorization
+from app.services.telegram_auth import verify_authorization, safe_parse_webapp_init_data
 from app.services.users import get_user, count_users, create_user, edit_user_status
 from app.utils.logging import logger
 
@@ -33,6 +33,74 @@ def login_redirect():
         return redirect('/')
 
     abort(400)
+
+
+@main.route('/login_web_app', methods=['GET', 'POST'])
+def login_web_app():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index', _external=True))
+
+    if not request.form or '_auth' not in request.form.to_dict():
+        content = """
+
+            <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+            <script src="https://telegram.org/js/telegram-web-app.js?1"></script>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+             integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+              crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+            <script>
+                Telegram.WebApp.ready();
+
+                $.ajax({
+                    type: 'POST',
+                    url: '/login_web_app',
+                    data: {_auth: Telegram.WebApp.initData},
+                    success: function(response){
+                        document.body.innerText = 'Вы успешно авторизовались, перезапустите страницу';
+                        window.location.href = response
+                    },
+                    error: function() {
+                        document.body.innerText = 'У вас нет прав';
+                    },
+                });
+
+            </script>
+        </head>
+        <body>
+        </body>
+        </html>
+
+            """
+
+        return Response(content, mimetype='text/html')
+
+    data = request.form.to_dict()
+
+    data = safe_parse_webapp_init_data(bot.token, data['_auth'])
+    if data:
+        data = data['user']
+        if not count_users():
+            create_user(data['id'], data['first_name'], data['username'])
+            edit_user_status(data['id'], 'super_admin')
+
+        user = get_user(id=data['id'])
+        if not user:
+            abort(400, 'This account is not present in the database')
+        if not user.is_admin():
+            abort(403, 'Admin panel is available only for administrators, contact the creator to get the rights')
+        login_user(user, remember=True)
+
+        logger.info(f'{user} logged')
+
+        return url_for('main.index', _external=True), 200
+    else:
+        abort(403, 'Fake user')
 
 
 @main.route('/login', methods=['GET', 'POST'])
--- app/services/telegram_auth.py
+++ app/services/telegram_auth.py
@@ -1,5 +1,53 @@
 import hashlib
 import hmac
+import json
+from operator import itemgetter
+from urllib.parse import parse_qsl
+
+
+def check_webapp_signature(token: str, init_data: str) -> bool:
+    try:
+        parsed_data = dict(parse_qsl(init_data, strict_parsing=True))
+    except ValueError:
+        return False
+    if "hash" not in parsed_data:
+        return False
+    hash_ = parsed_data.pop("hash")
+
+    data_check_string = "\n".join(
+        f"{k}={v}" for k, v in sorted(parsed_data.items(), key=itemgetter(0))
+    )
+    secret_key = hmac.new(key=b"WebAppData", msg=token.encode(), digestmod=hashlib.sha256)
+    calculated_hash = hmac.new(
+        key=secret_key.digest(), msg=data_check_string.encode(), digestmod=hashlib.sha256
+    ).hexdigest()
+    return calculated_hash == hash_
+
+
+def parse_webapp_init_data(
+        init_data: str,
+        *,
+        loads=json.loads,
+):
+    result = {}
+    for key, value in parse_qsl(init_data):
+        if (value.startswith("[") and value.endswith("]")) or (
+                value.startswith("{") and value.endswith("}")
+        ):
+            value = loads(value)
+        result[key] = value
+    return result
+
+
+def safe_parse_webapp_init_data(
+        token: str,
+        init_data: str,
+        *,
+        loads=json.loads,
+):
+    if check_webapp_signature(token, init_data):
+        return parse_webapp_init_data(init_data, loads=loads)
+    return False
 
 
 def verify_authorization(data: dict, api_token: str) -> bool:
